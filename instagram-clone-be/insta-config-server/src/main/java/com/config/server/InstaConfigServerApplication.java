e3f859ab0913e3950bc6e8ff29a1a9da1d78ce38
Jadhav, Vaibhav <vjadhav@microstrategy.com>
2023-12-10 00:03:04 +0530

    Config server implemented

--- instagram-clone-be/insta-config-server/src/main/java/com/config/server/InstaConfigServerApplication.java
+++ instagram-clone-be/insta-config-server/src/main/java/com/config/server/InstaConfigServerApplication.java
@@ -1,15 +1,17 @@
-package com.authorization.server;
+package com.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class InstaAuthorizationServerApplication {
+@EnableConfigServer
+public class InstaConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InstaAuthorizationServerApplication.class, args);
+		SpringApplication.run(InstaConfigServerApplication.class, args);
 	}
 
 }
